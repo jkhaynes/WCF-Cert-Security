8d928c72209ecae5ed908ed04425c9f8bf69ad9f
Jessica Haynes <jkhaynes2390@gmail.com>
2020-08-25 16:48:44 -0500

    hello world wcf service protected by cert

---
+++ SimpleAuthService/SimpleAuthService/Controllers/CertController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleAuthService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CertController : ControllerBase
+    {
+        // GET: api/<AuthController>
+        [HttpGet]
+        public X509Certificate Get()
+        {
+            return null;
+        }
+    }
+}
